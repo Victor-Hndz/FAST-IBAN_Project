a0337a3097f977f11a6641dc1d42eab73598d5a1
scolab_server <victor.hernandez06@goumh.umh.es>
2023-11-20 11:30:36 +0100

    inicio de mas tests

--- code/tests/test_great_circle.c
+++ code/tests/test_great_circle.c
@@ -3,10 +3,6 @@
 
 
 int main(void) {
-    coord_point point = create_point(1.0, 2.0);
-    assert(point.lat == 1.0);
-    assert(point.lon == 2.0);
-    
     printf("Test passed.\n");
     return 0;
 }
